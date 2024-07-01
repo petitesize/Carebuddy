1c9c056934d1b2000e44e93d829a36d12b4777da
chacha <yushin.pak@gmail.com>
2024-07-01 14:31:11 +0900

    Fix: 타입스크립트 오류 수정

--- src/components/Mypage&Userpage/PetCardContainer.tsx
+++ src/components/Mypage&Userpage/PetCardContainer.tsx
@@ -5,7 +5,7 @@ const Container = styled.div`
   margin: 30px 0 30px 0;
 `;
 
-const PetCardContainer = () => (
+const PetCardContainer:React.FC = () => (
   <Container>
     <div>반려동물 카드</div>
   </Container>
